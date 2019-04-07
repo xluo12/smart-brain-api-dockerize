36ba2cd035cab7dd872ce12057f42216d3a0ab88
Xiaoyu Luo <xluoforhire@gmail.com>
2019-04-07 18:23:19 -0400

    change username

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '897f7f1747284da582d377cdd6322eda'
 });
 
 const handleApiCall = (req, res) => {
