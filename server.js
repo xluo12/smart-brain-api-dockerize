a015d3d0f8d5300d8c991f7636ab6fd287c9fc16
Xiaoyu Luo <xluoforhire@gmail.com>
2019-04-09 12:55:14 -0400

    add morgan

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -22,6 +23,7 @@ const db = knex({
 const app = express();
 
 app.use(cors())
+app.use(morgan("combined"))
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send("It's working!!!") })
