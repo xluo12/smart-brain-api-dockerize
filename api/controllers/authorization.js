192aa2beb17c498bf23c974c74e877ba6075e99d
Xiaoyu Luo <xluoforhire@gmail.com>
2019-04-20 15:21:17 -0400

    before deleting libraries

--- api/controllers/authorization.js
+++ api/controllers/authorization.js
@@ -1,15 +1,24 @@
+const fetch = require('node-fetch');
+
 
 const requireAuth = (req, res, next) => {
     const { authorization } = req.headers;
     if (!authorization) {
         return res.status(401).json('Unauthorized');
     }
-    fetch('http://localhost:23456/', {
-        method: req.method,
-        headers: req.headers,
-        authorization: req.authorization
+    console.log("fetch!!!!!!!!!!!!!!!\n\n");
+    console.log("authorization: ", authorization);
+    fetch(process.env.AUTHORIZATION_URI, {
+        method: 'post',
+        headers: {'Content-Type': 'application/json',
+                  'Authorization': authorization
+        }
     })
-    .then(res => res.json())
+    .then(res => {
+        console.log('res: \n\n', res);
+        res.json();
+        console.log('res after json: \n\n', res);
+    })
     .then(res => {
         if (res.status === 401) {
             return res.status(401).json('Unauthorized');
--- authorization/auth.js
+++ authorization/auth.js
@@ -1,11 +1,17 @@
-const redisClient = require('../api/controllers/signin').redisClient;
+const redis = require('redis');
+
+// create Redis client
+const redisClient = redis.createClient(process.env.REDIS_URI);
 
 const authorize = (req, res) => {
     const { authorization } = req.headers;
+    console.log('received request: \n');
+    console.log('authorization: \n', authorization);
     return redisClient.get(authorization, (err, reply) => {
         if (err || !reply) {
             return res.status(401).json('Unauthorized');
         } else {
+            console.log('reply: \n', reply);
             return res.status(200).json('Works!');
         }   
     })
